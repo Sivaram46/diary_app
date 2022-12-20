3fd6dde7ac85cc0a492b6ba0bc537918f92b5786
Sivaram T <sivaram@Sivaram.local>
2022-12-20 23:29:40 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Diary App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DiaryHomePage(title: "Diary App")
+    );
+  }
+}
+
+class DiaryHomePage extends StatefulWidget {
+  const DiaryHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<DiaryHomePage> createState() => _DiaryHomePageState();
+}
+
+class _DiaryHomePageState extends State<DiaryHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+          child: Text(
+              "Hello World!"
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
