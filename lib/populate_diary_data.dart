e052ac1fa1eb2e3f6c36fc68a194c8bd392a2fd5
Sivaram T <t.s.ram46@gmail.com>
2023-01-15 14:00:48 +0530

    Added system generated files

--- lib/populate_diary_data.dart
+++ lib/populate_diary_data.dart
@@ -1,5 +1,20 @@
 import 'diary_model.dart';
 
+/*
+First diary entry
+-----------------
+This is my first diary entry I had written in this app. I am so glad to develop such app.
+
+Looking forward to make changes in the app and make it working.
+
+About by day
+------------
+Today I went to work as usual, they assigned me a bug and tried to fix the bug.
+
+Although that bug can be fixed in like 2 hours, I took almost the day. Played carrom, TT...
+
+*/
+
 final _someValues = <Diary>[
   Diary(createdDate: DateTime(2022, 1, 15), body: """At the same time that the scale and accuracy of deep networks has increased, so has the complexity of the tasks that they can solve. Goodfellow et al. (2014d) showed that neural networks could learn to output an entire sequence of characters transcribed from an image, rather than just identifying a single object. Previously, it was widely believed that this kind of learning required labeling of the individual elements of the sequence (Gülçehre and Bengio, 2013). Recurrent neural networks, such as the LSTM sequence model mentioned above, are now used to model relationships between sequences and other sequences rather than just fixed inputs.
 This sequence-to-sequence learning seems to be on the cusp of revolutionizing another application: machine translation (Sutskever et al., 2014; Bahdanau et al.,
